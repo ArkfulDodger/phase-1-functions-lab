8b0c2423ef4e45ce50d0b5de44802f258ee985fb
Noah Reece <noah.w.reece@gmail.com>
2022-02-22 22:07:48 -0500

    commit tree 8cac41224d305aeef7bac13bef2016801063967a

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(pickupLocation - 42);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+}
+
+function distanceTravelledInFeet(pickupLocation, dropoffLocation) {
+    return Math.abs(pickupLocation - dropoffLocation) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+
+    if (distance > 2500) {
+        return 'cannot travel that far';
+    } else if (distance > 2000) {
+        return 25;        
+    } else if (distance > 400) {
+        return (distance - 400) * 0.02;
+    } else {
+        return 0;
+    }
+}
